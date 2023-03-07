4573fd8183d0e6836948abfc2bfe81ea9f7c7832
Norse-Horse <122167559+Norse-Horse@users.noreply.github.com>
2023-03-07 11:59:04 -0500

    Create message.py

---
+++ message.py
@@ -0,0 +1,10 @@
+messages = 0
+import PySimpleGUI as sg
+window = sg.Window(title="Message Maker", layout=[[sg.InputText('Insert some Input Text!')], [sg.Button('Done'), sg.Button('Exit')], margins=(350, 150)).read()
+messages += 1
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    window = sg.Window(title="Message" + str(messages), layout=[[sg.InputText(values[0])], [sg.Button('Close')]], margins=(100, 50)).read()
+    messages += 1
